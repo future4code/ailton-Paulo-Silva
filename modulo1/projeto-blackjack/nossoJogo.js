b247550bb7e70b9b4bd9e939a1084a44bbcf8171
Paulo Victor de Oliveira Silva <p2.paulovictor.p2@gmail.com>
2022-05-20 23:16:51 -0300

    projeto-blackjack alteracao

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -1,26 +1,66 @@
+// console.log("Boas vindas ao jogo de Blackjack!");
+
+// if (confirm("Quer iniciar uma nova rodada?")) {
+//   const primeriaCartaUsuario = comprarCarta();
+//   const segundaCartaUsuario = comprarCarta();
+//   const primeiraCartaComputador = comprarCarta();
+//   const segundaCartaComputador = comprarCarta();
+
+//   console.log(`Usuário - cartas: ${primeriaCartaUsuario.texto} ${
+//     segundaCartaUsuario.texto
+//   }  - pontuação ${primeriaCartaUsuario.valor + segundaCartaUsuario.valor}
+// Computador - cartas: ${primeiraCartaComputador.texto} ${
+//     segundaCartaComputador.texto
+//   }  - pontuação ${primeiraCartaComputador.valor + segundaCartaComputador.valor}
+//    `);
+//   if (
+//     primeriaCartaUsuario.valor + segundaCartaUsuario.valor ===
+//     primeiraCartaComputador.valor + segundaCartaComputador.valor
+//   ) {
+//     console.log("Empate!");
+//   } else if (
+//     primeriaCartaUsuario.valor + segundaCartaUsuario.valor >
+//     primeiraCartaComputador.valor + segundaCartaComputador.valor
+//   ) {
+//     console.log("O usuário ganhou!");
+//   } else {
+//     console.log("O computador ganhou!");
+//   }
+// } else {
+//   console.log("O jogo acabou");
+// }
+
 console.log("Boas vindas ao jogo de Blackjack!");
 
 if (confirm("Quer iniciar uma nova rodada?")) {
-  const primeriaCartaUsuario = comprarCarta();
-  const segundaCartaUsuario = comprarCarta();
-  const primeiraCartaComputador = comprarCarta();
-  const segundaCartaComputador = comprarCarta();
+  const cartasUsuario = {
+    primeraCarta: comprarCarta(),
+    segundaCarta: comprarCarta(),
+  };
+  const cartasComputador = {
+    primeraCarta: comprarCarta(),
+    segundaCarta: comprarCarta(),
+  };
 
-  console.log(`Usuário - cartas: ${primeriaCartaUsuario.texto} ${
-    segundaCartaUsuario.texto
-  }  - pontuação ${primeriaCartaUsuario.valor + segundaCartaUsuario.valor}
-Computador - cartas: ${primeiraCartaComputador.texto} ${
-    segundaCartaComputador.texto
-  }  - pontuação ${primeiraCartaComputador.valor + segundaCartaComputador.valor}
+  console.log(`Usuário - cartas: ${cartasUsuario.primeraCarta.texto} ${
+    cartasUsuario.segundaCarta.texto
+  }  - pontuação ${
+    cartasUsuario.primeraCarta.valor + cartasUsuario.segundaCarta.valor
+  }
+Computador - cartas: ${cartasComputador.primeraCarta.texto} ${
+    cartasComputador.segundaCarta.texto
+  }  - pontuação ${
+    cartasComputador.primeraCarta.valor + cartasComputador.segundaCarta.valor
+  }
    `);
   if (
-    primeriaCartaUsuario.valor + segundaCartaUsuario.valor ===
-    primeiraCartaComputador.valor + segundaCartaComputador.valor
+    cartasUsuario.primeraCarta.valor + cartasUsuario.segundaCarta.valor ===
+    cartasComputador.primeraCarta.valor + cartasComputador.segundaCarta.valor
   ) {
     console.log("Empate!");
   } else if (
-    primeriaCartaUsuario.valor + segundaCartaUsuario.valor >
-    primeiraCartaComputador.valor + segundaCartaComputador.valor
+    cartasUsuario.primeraCarta.valor + cartasUsuario.segundaCarta.valor >
+    cartasComputador.primeraCarta.valor + cartasComputador.segundaCarta.valor
   ) {
     console.log("O usuário ganhou!");
   } else {
